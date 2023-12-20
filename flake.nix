72fbd7409da9f8fbbb67839a8e6121a8d9836f4d
Mr. Fluffy <git@sixearsm>
2023-12-20 09:39:41 +0000

    use 2023-12-13; ghc9+hpkgs r0.0.24.0

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "nix configuration for X-based things";
 
   inputs = {
-    nixpkgs.url     = github:nixos/nixpkgs/be44bf67; # nixos-22.05 2022-10-15
+    nixpkgs.url = github:NixOS/nixpkgs/354184a; # master 2023-12-13
     flake-utils.url = github:numtide/flake-utils/c0e246b9;
     myPkgs          = {
       url    = github:sixears/nix-pkgs/r0.0.1.3;
